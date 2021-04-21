70105d723899e1aa6b00115594668c206776657b
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-04-21 10:40:24 +0200

    Basic structure added

---
+++ hero.php
@@ -0,0 +1,10 @@
+<div class="hero container-fluid">
+    <div class="row h-100 align-items-center">
+        <div class="offset-1 col-8">
+            <h1 class="display-3 bg-darker">Dannys Delicious Dishes</h1>
+        </div>
+        <div class="offset-1 col-8">
+            <h2 class="display-5 bg-darker">OMG.🙀 Its sooo delicious!</h2>
+        </div>
+    </div>
+</div>
