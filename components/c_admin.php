dab9918e9e035527f86013201e394df4f8ec632f
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-04-23 17:41:37 +0200

    Admin Page modified

--- components/c_admin.php
+++ components/c_admin.php
@@ -1,42 +1,40 @@
 <?php
     require_once 'actions/db_connect.php';
-    $sql = "SELECT mid,title,isbn,`type` FROM media";
+    $sql = "SELECT mid,title,isbn FROM media";
     $fetch_result = $mysqli->query($sql);
     $mysqli->close();
 ?>
-
+<nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Admin</li>
+    </ol>
+</nav>
 <table class="table">
     <thead>
         <tr>
-            <th scope="col">id</th>
-            <th scope="col">title</th>
-            <th scope="col">isbn</th>
-            <th scope="col">type</th>
             <th scope="col"></th>
+            <th scope="col">ID</th>
+            <th scope="col">Title</th>
+            <th scope="col">ISBN</th>   
         </tr>
     </thead>
     <tbody>
         <tr>
-            <form action="modify.php" method="GET">
-                <td><input type="text" class="form-control" name="name" placeholder="name"></td>
-                <td><input type="text" class="form-control" name="price" placeholder="price"></td>
-                <td><input type="text" class="form-control" name="description" placeholder="description"></td>
-                <td><input type="url" class="form-control" name="img_url" placeholder="url"></td>
-                <td><button type="submit" name="action" class="btn btn-success btn-sm" value="add"><i class="bi bi-plus-square"></i></button></td>
-            </form>
+            <td><a href="#" class="bi bi-plus-square fs-4 text-success "></a></td>
+            <td colspan="4">Add new media item</td>     
         </tr>
         <?php 
             while ($media = $fetch_result->fetch_assoc()) {
                 echo '
                     <tr>
+                        <td>
+                            <a href="delete.php?action=modify&id='.$media["id"].'" class="bi bi-dash-square text-danger fs-4"></a>
+                            <a href="modify.php?id='.$media["id"].'" class="bi bi-pencil-square text-secondary fs-4"></a>
+                        </td>
                         <td>'.$media["mid"].'</td>
                         <td>'.$media["title"].'</td>
                         <td>'.$media["isbn"].'</td>
-                        <td>'.$media["type"].'</td>
-                        <td>
-                            <a href="modify.php?action=modify&id='.$media["id"].'" class="btn btn-danger btn-sm"><i class="bi bi-dash-square"></i></a>
-                            <a href="delete.php?id='.$media["id"].'" class="btn btn-secondary btn-sm"><i class="bi bi-pencil-square"></i></a>
-                        </td>
                     </tr>';
             }
         ?>   
