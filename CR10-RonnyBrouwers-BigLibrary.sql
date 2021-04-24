63d3262b2a8ac589125d88e5476e52557f490c5a
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-04-24 14:05:52 +0200

    Database data adapted | on delete cascade added

--- CR10-RonnyBrouwers-BigLibrary.sql
+++ CR10-RonnyBrouwers-BigLibrary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 23. Apr 2021 um 11:09
+-- Erstellungszeit: 24. Apr 2021 um 14:01
 -- Server-Version: 10.4.18-MariaDB
 -- PHP-Version: 8.0.3
 
@@ -79,36 +79,31 @@ CREATE TABLE `media` (
 --
 
 INSERT INTO `media` (`mid`, `title`, `image`, `isbn`, `description`, `pub_date`, `isAvailable`, `type`, `pid`) VALUES
-(0000000001, 'maximize back-end bandwidth', 'http://dummyimage.com/461x255.png/cc0000/ffffff', '939882294-6', 'Fusce consequat. Nulla nisl. Nunc nisl.', '2009-06-13', 0, 'cd', 0000000005),
-(0000000002, 'revolutionize compelling web services', 'http://dummyimage.com/325x464.png/dddddd/000000', '090130573-1', 'Duis consequat dui nec nisi volutpat eleifend.', '2019-12-22', 1, 'cd', 0000000003),
-(0000000003, 'embrace killer schemas', 'http://dummyimage.com/266x366.png/ff4444/ffffff', '387082462-X', 'Vivamus vestibulum sagittis sapien.', '2019-06-08', 0, 'book', 0000000001),
-(0000000004, 'utilize sticky synergies', 'http://dummyimage.com/381x496.png/cc0000/ffffff', '175582512-9', 'Etiam pretium iaculis justo. In hac habitasse platea dictumst.', '2017-08-11', 0, 'dvd', 0000000003),
-(0000000005, 'visualize plug-and-play eyeballs', 'http://dummyimage.com/479x495.png/5fa2dd/ffffff', '544353230-8', 'Vivamus tortor.', '2014-04-16', 0, 'cd', 0000000007),
-(0000000006, 'benchmark revolutionary relationships', 'http://dummyimage.com/379x310.png/ff4444/ffffff', '629969551-X', 'Curabitur gravida nisi at nibh.', '2006-08-23', 1, 'dvd', 0000000001),
-(0000000007, 'grow intuitive content', 'http://dummyimage.com/281x490.png/cc0000/ffffff', '544090093-4', 'Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '2017-07-15', 1, 'book', 0000000010),
-(0000000008, 'synthesize scalable channels', 'http://dummyimage.com/493x283.png/dddddd/000000', '892277151-8', 'Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', '2019-05-28', 0, 'dvd', 0000000010),
-(0000000009, 'optimize B2C deliverables', 'http://dummyimage.com/481x429.png/dddddd/000000', '491978166-0', 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.', '2009-01-25', 1, 'book', 0000000003),
-(0000000010, 'incubate e-business paradigms', 'http://dummyimage.com/294x495.png/ff4444/ffffff', '247519464-2', 'Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.', '2020-07-17', 0, 'cd', 0000000004),
-(0000000011, 'engage open-source technologies', 'http://dummyimage.com/384x446.png/cc0000/ffffff', '576403633-X', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue.', '2015-07-31', 0, 'book', 0000000009),
-(0000000012, 'syndicate next-generation synergies', 'http://dummyimage.com/408x448.png/ff4444/ffffff', '090840491-3', 'Quisque id justo sit amet sapien dignissim vestibulum.', '2013-02-23', 0, 'cd', 0000000010),
-(0000000013, 'transform wireless deliverables', 'http://dummyimage.com/304x467.png/dddddd/000000', '018435669-5', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', '2012-05-31', 1, 'book', 0000000001),
-(0000000014, 'innovate vertical metrics', 'http://dummyimage.com/291x446.png/5fa2dd/ffffff', '984937023-8', 'Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl.', '2017-02-24', 1, 'book', 0000000005),
-(0000000015, 'brand holistic schemas', 'http://dummyimage.com/458x264.png/dddddd/000000', '561361622-1', 'Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.', '2014-05-31', 0, 'book', 0000000001),
-(0000000016, 'leverage leading-edge deliverables', 'http://dummyimage.com/285x284.png/dddddd/000000', '971322155-9', 'Maecenas rhoncus aliquam lacus.', '2009-11-28', 0, 'dvd', 0000000007),
-(0000000017, 'orchestrate collaborative markets', 'http://dummyimage.com/269x363.png/cc0000/ffffff', '181635668-9', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem.', '2020-01-26', 0, 'book', 0000000001),
-(0000000018, 'integrate ubiquitous e-services', 'http://dummyimage.com/476x336.png/5fa2dd/ffffff', '072858108-6', 'Morbi non quam nec dui luctus rutrum.', '2009-07-03', 1, 'dvd', 0000000007),
-(0000000019, 'synthesize B2B partnerships', 'http://dummyimage.com/379x480.png/dddddd/000000', '051776954-9', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue.', '2013-08-28', 0, 'dvd', 0000000004),
-(0000000020, 'visualize vertical e-markets', 'http://dummyimage.com/434x399.png/ff4444/ffffff', '104323308-3', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.', '2018-11-20', 0, 'dvd', 0000000001),
-(0000000021, 'monetize next-generation methodologies', 'http://dummyimage.com/314x287.png/5fa2dd/ffffff', '380474465-6', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', '2007-01-22', 1, 'book', 0000000010),
-(0000000022, 'enhance intuitive action-items', 'http://dummyimage.com/429x293.png/cc0000/ffffff', '437659282-X', 'Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', '2008-10-16', 1, 'dvd', 0000000007),
-(0000000023, 'drive 24/365 technologies', 'http://dummyimage.com/297x475.png/5fa2dd/ffffff', '604792349-6', 'Proin at turpis a pede posuere nonummy.', '2011-03-04', 0, 'dvd', 0000000010),
-(0000000024, 'deploy B2B supply-chains', 'http://dummyimage.com/481x476.png/ff4444/ffffff', '319578883-7', 'Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.', '2014-01-20', 1, 'cd', 0000000009),
-(0000000025, 'generate impactful applications', 'http://dummyimage.com/282x296.png/cc0000/ffffff', '702029209-7', 'Donec quis orci eget orci vehicula condimentum.', '2010-07-03', 1, 'book', 0000000004),
-(0000000026, 'target collaborative interfaces', 'http://dummyimage.com/466x383.png/dddddd/000000', '182035472-5', 'Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit.', '2009-01-25', 0, 'book', 0000000001),
-(0000000027, 'mesh turn-key functionalities', 'http://dummyimage.com/276x477.png/cc0000/ffffff', '545217435-4', 'Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.', '2017-07-05', 1, 'dvd', 0000000007),
-(0000000028, 'target value-added e-markets', 'http://dummyimage.com/413x448.png/5fa2dd/ffffff', '398127196-3', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', '2018-02-20', 0, 'book', 0000000009),
-(0000000029, 'productize efficient e-markets', 'http://dummyimage.com/268x293.png/5fa2dd/ffffff', '688428166-9', 'Morbi ut odio.', '2009-06-20', 0, 'book', 0000000007),
-(0000000030, 'innovate extensible interfaces', 'http://dummyimage.com/433x391.png/dddddd/000000', '837461928-7', 'Mauris sit amet eros. Suspendisse accumsan tortor quis turpis. Sed ante.', '2020-10-21', 1, 'cd', 0000000009);
+(0000000001, 'Darkness on the Edge of Town', 'https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/07/54da4f80dd7af_-_springsteen-darkness-edge-l-30776928.jpg', '939882294-6', 'Fusce consequat. Nulla nisl. Nunc nisl.', '2009-06-13', 0, 'cd', 0000000005),
+(0000000002, 'Phases and Stages', 'https://hips.hearstapps.com/esq.h-cdn.co/assets/cm/15/05/54cb1f91dd0e3_-_willie-nelson-phases-stages-2009-lg-90129818.jpg', '090130573-1', 'Duis consequat dui nec nisi volutpat eleifend.', '2019-12-22', 1, 'cd', 0000000003),
+(0000000003, 'To Kill a Mockingbird', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/50-anniversary-cover1.jpg', '387082462-X', 'Vivamus vestibulum sagittis sapien.', '2019-06-08', 0, 'book', 0000000001),
+(0000000004, 'The Godfather', 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg', '175582512-9', 'Etiam pretium iaculis justo. In hac habitasse platea dictumst.', '2017-08-11', 0, 'dvd', 0000000003),
+(0000000005, 'Rumours ', 'https://img.buzzfeed.com/buzzfeed-static/static/2017-07/12/10/asset/buzzfeed-prod-fastlane-03/sub-buzz-10383-1499869515-10.jpg', '544353230-8', 'Vivamus tortor.', '2014-04-16', 0, 'cd', 0000000007),
+(0000000006, 'Do the Right Thing', 'https://musicart.xboxlive.com/7/e50b1100-0000-0000-0000-000000000002/504/image.jpg', '629969551-X', 'Curabitur gravida nisi at nibh.', '2006-08-23', 1, 'dvd', 0000000001),
+(0000000007, '1984', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/1984.jpg', '544090093-4', 'Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', '2017-07-15', 1, 'book', 0000000010),
+(0000000008, 'Citizen Kane', 'https://images-na.ssl-images-amazon.com/images/I/91zcYtPPMOL._SY679_.jpg', '892277151-8', 'Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', '2019-05-28', 0, 'dvd', 0000000010),
+(0000000009, 'Harry Potter and the Philosopher\'s Stone', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/harry_potter_and_the_Sorcerers_stone_adult_usa.jpg', '491978166-0', 'Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.', '2009-01-25', 1, 'book', 0000000003),
+(0000000010, 'C\'est Si Bon ', 'https://img.buzzfeed.com/buzzfeed-static/static/2017-07/12/11/asset/buzzfeed-prod-fastlane-03/sub-buzz-16239-1499872338-1.jpg', '247519464-2', 'Morbi vel lectus in quam fringilla rhoncus. Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis.', '2020-07-17', 0, 'cd', 0000000004),
+(0000000011, 'The Lord of the Rings', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/9780618640157_custom-s6-c30.jpg', '576403633-X', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue.', '2015-07-31', 0, 'book', 0000000009),
+(0000000012, 'Is This It', 'https://img.buzzfeed.com/buzzfeed-static/static/2017-07/12/12/asset/buzzfeed-prod-fastlane-03/sub-buzz-20678-1499877453-1.jpg', '090840491-3', 'Quisque id justo sit amet sapien dignissim vestibulum.', '2013-02-23', 0, 'cd', 0000000010),
+(0000000013, 'The Great Gatsby', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/Penguin-2.jpg', '018435669-5', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', '2012-05-31', 1, 'book', 0000000001),
+(0000000014, 'Pride and Prejudice', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/pride_and_prejudice_book_cover_by_fourblackbirds-d533108.png', '984937023-8', 'Morbi non quam nec dui luctus rutrum. Nulla tellus. In sagittis dui vel nisl.', '2017-02-24', 1, 'book', 0000000005),
+(0000000015, 'The Diary Of A Young Girl', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/diary-of-anne-frank-postcard-front_0.jpg', '561361622-1', 'Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.', '2014-05-31', 0, 'book', 0000000001),
+(0000000016, 'Before Sunrise', 'https://images-na.ssl-images-amazon.com/images/I/91nT75kyfmL._SL1500_.jpg', '971322155-9', 'Maecenas rhoncus aliquam lacus.', '2009-11-28', 0, 'dvd', 0000000007),
+(0000000017, 'The Book Thief', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/71h2sjik5al-_sl1380_.jpg', '181635668-9', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem.', '2020-01-26', 0, 'book', 0000000001),
+(0000000018, 'Boyhood', 'https://bloximages.chicago2.vip.townnews.com/tucson.com/content/tncms/assets/v3/editorial/2/1c/21c68bbe-08cb-11e4-9c3a-0019bb2963f4/53bf8f0e9775b.image.jpg', '072858108-6', 'Morbi non quam nec dui luctus rutrum.', '2009-07-03', 1, 'dvd', 0000000007),
+(0000000019, '2001: A Space Odyssey', 'https://upload.wikimedia.org/wikipedia/en/1/11/2001_A_Space_Odyssey_%281968%29.png', '051776954-9', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue.', '2013-08-28', 0, 'dvd', 0000000004),
+(0000000020, 'The Rules of the Game', 'https://images-na.ssl-images-amazon.com/images/I/514quGu-sYL._SY445_.jpg', '104323308-3', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.', '2018-11-20', 0, 'dvd', 0000000001),
+(0000000021, 'Fahrenheit 451', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/tumblr_nd4wnpO3ZS1tv8vcro1_r1_1280.jpg', '380474465-6', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', '2007-01-22', 1, 'book', 0000000010),
+(0000000022, 'Toy Story', 'https://www.labels4kids.com/media/catalog/product/t/o/toy_story_collection_cover_standard_cover_art.jpg', '437659282-X', 'Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', '2008-10-16', 1, 'dvd', 0000000007),
+(0000000023, 'Psycho', 'https://i.pinimg.com/originals/71/18/35/7118350d182be5c06b1c9a6f5b2450a8.jpg', '604792349-6', 'Proin at turpis a pede posuere nonummy.', '2011-03-04', 0, 'dvd', 0000000010),
+(0000000024, 'Hotel California', 'https://img.buzzfeed.com/buzzfeed-static/static/2017-07/12/10/asset/buzzfeed-prod-fastlane-02/sub-buzz-32276-1499868759-3.jpg', '319578883-7', 'Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.', '2014-01-20', 1, 'cd', 0000000009),
+(0000000025, 'Jane Eyre', 'https://cdn.lifehack.org/wp-content/uploads/2015/03/cvr9781416500247_9781416500247_hr.jpg', '702029209-7', 'Donec quis orci eget orci vehicula condimentum.', '2010-07-03', 1, 'book', 0000000004);
 
 -- --------------------------------------------------------
 
@@ -265,7 +260,7 @@ ALTER TABLE `media`
 --
 ALTER TABLE `media_author`
   ADD CONSTRAINT `fk_media_author_author` FOREIGN KEY (`aid`) REFERENCES `author` (`aid`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_media_author_media` FOREIGN KEY (`mid`) REFERENCES `media` (`mid`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_media_author_media` FOREIGN KEY (`mid`) REFERENCES `media` (`mid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
