aff9e282075abf68c6ebe8229b52add18d435302
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-04-30 20:58:52 +0200

    Minor logo adaption

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid mt-4">
     <div class="row">
         <div class="col-12">
-            <img class="logo d-block mx-auto" src="img/logo.jpg" alt="logo">
+            <img class="logo d-block mx-auto" src="img/logo.png" alt="logo">
         </div>
         <div class="col-12">
             <p class="text-muted text-center p-3">© 2021 Copyright: Petship</p>  
--- components/nav.php
+++ components/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 	<div class="container">
     	<a class="navbar-brand" href="index.php">
-      		<img class="logo" src="img/logo.jpg" alt="logo">
+      		<img class="logo" src="img/logo.png" alt="logo">
     	</a>
     	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       		<span class="navbar-toggler-icon"></span>
